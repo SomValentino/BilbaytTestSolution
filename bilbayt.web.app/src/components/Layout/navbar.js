1e436a89a8082a31fde2afd63682c979649e867a
Valentine <valazom@gmail.com>
2021-07-02 13:37:47 +0200

    resoled navigation bug

--- bilbayt.web.app/src/components/Layout/navbar.js
+++ bilbayt.web.app/src/components/Layout/navbar.js
@@ -1,16 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from './navbar.module.css'
+import {useHistory} from 'react-router-dom'
+import {useState} from 'react'
 const Navbar = () => {
+  const history = useHistory()
+  const [path, setPath] = useState(history.location.pathname)
+
+  const handleClick = () => {
+      let nextPath = ''
+      if(path === '/' || path === '/login') nextPath = '/register'
+      else nextPath ='/'
+      history.push(nextPath)
+      setPath(nextPath)
+  }
   return (
     <header className={classes.header}>
       <h1>Bilbayt</h1>
       <nav>
         <ul>
           <li>
-            <Link className={classes.button} to="/register">
-              <span className={classes.badge}>Register</span>
-            </Link>
+            {path === "/" || path === "/login" ? (
+              <Link className={classes.button} onClick={handleClick}>
+                <span className={classes.badge}>Register</span>
+              </Link>
+            ) : path === "/register" ? (
+              <Link className={classes.button} onClick={handleClick}>
+                <span className={classes.badge}>Login</span>
+              </Link>
+            ) : (
+              <a className={classes.button} onClick={handleClick}>
+                <span className={classes.badge}>Logout</span>
+              </a>
+            )}
           </li>
         </ul>
       </nav>
