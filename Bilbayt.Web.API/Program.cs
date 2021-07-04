f92cbfb3f726e99be949e1949c59301ef3d45b69
Valentine <valazom@gmail.com>
2021-07-04 13:08:50 +0200

    added logging

--- Bilbayt.Web.API/Program.cs
+++ Bilbayt.Web.API/Program.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,11 +15,48 @@ namespace Bilbayt.Web.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(hostContext.HostingEnvironment.ContentRootPath);
+                    configApp.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    configApp.AddEnvironmentVariables();
+
+                    var _config = configApp.Build();
+
+
+                    var fileName = Path.Combine("logs", _config.GetValue<string>("applicantLogFile"));
+                    Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .MinimumLevel.Debug()
+                        .WriteTo.Console()
+                        .WriteTo.File(fileName)
+                        .CreateLogger();
+
+                })
+                .UseSerilog()
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddFilter("Microsoft", LogLevel.Information)
+                           .AddFilter("System", LogLevel.Error);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
